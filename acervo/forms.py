7fa8dfebd44022f8caa42c50b7f9dd47a2983a2d
Luis Souza <luishcs03@gmail.com>
2026-09-16 20:59:19 -0300

    Cria BuscaForm para pesquisa por título, tipo e categoria

--- acervo/forms.py
+++ acervo/forms.py
@@ -5,3 +5,14 @@ class LivroForm(forms.ModelForm):
     class Meta:
         model = Livro
         fields = ['titulo', 'autor', 'ano', 'tipo', 'categoria']
+
+class BuscaForm(forms.Form):
+    titulo = forms.CharField(label='Título', required=False)
+    tipo = forms.ChoiceField(
+        label='Tipo', required=False,
+        choices=[('', 'Todos')] + Livro.Tipo.choices,
+    )
+    categoria = forms.ChoiceField(
+        label='Categoria', required=False,
+        choices=[('', 'Todas')] + Livro.Categoria.choices,
+    )
