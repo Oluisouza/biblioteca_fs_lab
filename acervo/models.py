6e1621fdb071cb78f672f07ae350eb17dc06e44d
Luis Souza <luishcs03@gmail.com>
2026-09-16 20:46:06 -0300

    Adiciona campo tipo (Físico/Digital ao Livro

--- acervo/models.py
+++ acervo/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 class Livro(models.Model):
+
+    class Tipo(models.TextChoices):
+        FISICO = 'fisico', 'Físico'
+        DIGITAL = 'digital', 'Digital'
+
     titulo = models.CharField(max_length=200)
     autor = models.CharField(max_length=100)
     ano = models.IntegerField()
     disponivel = models.BooleanField(default=True)
+    tipo = models.CharField(max_length=7, choices=Tipo.choices, default=Tipo.FISICO)
 
     def __str__(self):
         return self.titulo
