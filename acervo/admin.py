a24b7008dd5ee8775d68da5c22ee519c88523fee
Luis Souza <luishcs03@gmail.com>
2026-09-16 21:03:20 -0300

    Configura Admin com colunas, filtros e busca

--- acervo/admin.py
+++ acervo/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Livro
 
-admin.site.register(Livro)
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'autor', 'ano', 'tipo', 'categoria', 'disponivel']
+    list_filter = ['tipo', 'categoria', 'disponivel']
+    search_fields = ['titulo', 'autor']
